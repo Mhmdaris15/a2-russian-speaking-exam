753f2fea41c99ef42e38bcc0b45ee0ea278b2ab4
Muhammad Aris Septanugroho <muhammadaris1945@gmail.com>
2026-05-07 04:16:29 +0300

    Add GitHub Actions deploy to Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/a2-russian-speaking-exam">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
